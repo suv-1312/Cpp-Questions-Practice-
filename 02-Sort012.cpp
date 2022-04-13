f0c22815cdbec66031d02695c7c2ce8c31af1363
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2022-04-13 17:10:23 +0530

    Add files via upload

---
+++ 02-Sort012.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    private :
+    void swap(int *a,int*b)
+    {
+        int t = *a;
+        *a = *b;
+        *b = t;
+    }
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int n = nums.size();
+        int low =0,mid=0,high = n-1;
+        
+        while(mid<=high)
+        {
+           if(nums[mid] == 0)
+           {
+               swap(&nums[low],&nums[mid]);
+               low++;
+               mid++;
+           }
+            
+            else if(nums[mid] == 2)
+            {
+                swap(&nums[high],&nums[mid]);
+                high--;
+            }
+            else
+                mid++;
+        }
+    }
+};
