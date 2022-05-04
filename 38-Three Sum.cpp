ee0818a361293d3cd08b46e2aeae04d507938556
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2022-05-04 14:13:38 +0530

    Update 38-Three Sum.cpp

--- 38-Three Sum.cpp
+++ 38-Three Sum.cpp
@@ -22,42 +22,29 @@ Output: []
 */
 
 
-//-----------------APPROACH - 1------------------------------------
+//-----------------BRUTE FORCE------------------------------------
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int n = nums.size();
-        for(int i = 0 ; i<n ; i++)
-        {
-            m[nums[i]]++;
+vector < vector < int >> threeSum(vector < int > & nums) {
+  vector < vector < int >> ans;
+  vector < int > temp;
+  int i, j, k;
+  for (i = 0; i < nums.size() - 2; i++) {
+    for (j = i + 1; j < nums.size() - 1; j++) {
+      for (k = j + 1; k < nums.size(); k++) {
+        temp.clear();
+        if (nums[i] + nums[j] + nums[k] == 0) {
+          temp.push_back(nums[i]);
+          temp.push_back(nums[j]);
+          temp.push_back(nums[k]);
         }
-        vector<vector<int>> ans;
-        for(int i = 0; i< n ;i++)
-        { 
-            m[nums[i]]--;
-            for(int j = i+1 ; j<n; j++)
-            {
-                m[nums[j]]--;
-                int sum = (nums[i]+nums[j]);
-                if(m.find(0-sum)!=m.end())
-                {
-                    vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[j]);
-                    temp.push_back(sum);
-                    
-                    s.insert(temp);
-                }
-                m[nums[j]]++;
-
-            }
-            m[nums[i]]++;
-        }
-        return ans;
+        if (temp.size() != 0)
+          ans.push_back(temp);
+      }
     }
-};
+  }
+
+  return ans;
+}
 
 //-----------------APPROACH - 2------------------------------------
 
